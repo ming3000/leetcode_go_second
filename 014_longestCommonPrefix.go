8298e57f4b97d5da967d8e5adbd41b8f4bbd401f
xushouming <xushouming@bytedance.com>
2020-10-22 22:41:22 +0800

    014

---
+++ 014_longestCommonPrefix.go
@@ -0,0 +1,20 @@
+package leetcode_go_second
+
+import "strings"
+
+func longestCommonPrefix(strs []string) string {
+	if len(strs) == 0 {
+		return ""
+	}
+
+	prefix := strs[0]
+	for i := 1; i < len(strs); i++ {
+		for strings.Index(strs[i], prefix) != 0 {
+			if prefix == "" {
+				return ""
+			} // if>>>
+			prefix = prefix[:len(prefix)-1]
+		} // for>>
+	} // for>
+	return prefix
+}
