3d45b05840ed6bdfe882bc525142da52c47d7de6
xushouming <xushouming@bytedance.com>
2020-10-23 10:13:21 +0800

    026

---
+++ 026_removeDuplicates.go
@@ -0,0 +1,16 @@
+package leetcode_go_second
+
+func removeDuplicates(nums []int) int {
+	if len(nums) <= 1 {
+		return len(nums)
+	}
+	rightIndex := 1
+	for i := 1; i < len(nums); i++ {
+		if nums[i] != nums[i-1] {
+			nums[rightIndex] = nums[i]
+			rightIndex += 1
+		}
+	} // for>
+	retNums := nums[:rightIndex]
+	return len(retNums)
+}
