cd6e3fa74c0b8e0a3db184831622a29c38abad1c
xushouming <xushouming@bytedance.com>
2020-10-23 22:40:43 +0800

    300

--- 300_lengthOfLIS.go
+++ 300_lengthOfLIS.go
@@ -1,1 +1,23 @@
 package leetcode_go_second
+
+func lengthOfLIS(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+
+	dp := make([]int, len(nums))
+	for i := 0; i < len(dp); i++ {
+		dp[i] = 1
+	}
+
+	var ret int
+	for i := 0; i < len(nums); i++ {
+		for j := 0; j < i; j++ {
+			if nums[i] > nums[j] {
+				dp[i] = maxInt(dp[i], dp[j]+1)
+			} // if>>>
+		} // for>>
+		ret = maxInt(ret, dp[i])
+	} // for>
+	return ret
+}
