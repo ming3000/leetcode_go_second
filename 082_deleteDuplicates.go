b90cd7d6f6fc70512fae204c80d7dc07b1da62fd
xushouming <xushouming@bytedance.com>
2020-10-23 17:00:40 +0800

    082

---
+++ 082_deleteDuplicates.go
@@ -0,0 +1,18 @@
+package leetcode_go_second
+
+func deleteDuplicates(head *ListNode) *ListNode {
+	sentry := &ListNode{Next: head}
+	pre, cur := sentry, head
+	for cur != nil {
+		for cur.Next != nil && cur.Val == cur.Next.Val {
+			cur = cur.Next
+		} // for>>
+		if pre.Next != cur {
+			pre.Next = cur.Next
+		} else {
+			pre = cur
+		} // else>>
+		cur = cur.Next
+	} // for>
+	return sentry.Next
+}
