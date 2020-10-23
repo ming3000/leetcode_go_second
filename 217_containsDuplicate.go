1d5e3fd82fb392f72361c77fdb39ee45f992095b
xushouming <xushouming@bytedance.com>
2020-10-23 18:53:55 +0800

    217

---
+++ 217_containsDuplicate.go
@@ -0,0 +1,16 @@
+package leetcode_go_second
+
+import "sort"
+
+func containsDuplicate(nums []int) bool {
+	if len(nums) < 2 {
+		return false
+	}
+	sort.Ints(nums)
+	for i := 1; i < len(nums); i++ {
+		if nums[i] == nums[i-1] {
+			return true
+		} // if>>
+	} // for>
+	return false
+}
