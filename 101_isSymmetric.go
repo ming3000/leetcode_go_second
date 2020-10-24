d3600fd00c92dc5928d20e45c5d2122a1a633726
xushouming <xushouming@bytedance.com>
2020-10-24 09:59:13 +0800

    101

---
+++ 101_isSymmetric.go
@@ -0,0 +1,26 @@
+package leetcode_go_second
+
+func isSymmetric(root *TreeNode) bool {
+	u, v := root, root
+	queue := make([]*TreeNode, 0)
+	queue = append(queue, []*TreeNode{u, v}...)
+
+	for len(queue) > 0 {
+		u, v := queue[0], queue[1]
+		queue = queue[2:]
+		if u == nil && v == nil {
+			continue
+		} // if>>
+		if u == nil || v == nil {
+			return false
+		} // if>>
+		if u.Val != v.Val {
+			return false
+		} // if>>
+		queue = append(queue, u.Left)
+		queue = append(queue, v.Right)
+		queue = append(queue, u.Right)
+		queue = append(queue, v.Left)
+	} // for>
+	return true
+}
