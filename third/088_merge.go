c9e2796c4ed757ffe8c3abb285c88b4fdd0d8cfb
xushouming <xushouming@bytedance.com>
2020-10-25 22:13:47 +0800

    third start

--- third/088_merge.go
+++ third/088_merge.go
@@ -1,5 +1,15 @@
 package leetcode_go_third
 
 func merge(nums1 []int, m int, nums2 []int, n int) {
-
+	retIndex, i, j := m+n-1, m-1, n-1
+	for j >= 0 {
+		if i >= 0 && nums1[i] > nums2[j] {
+			nums1[retIndex] = nums1[i]
+			i--
+		} else {
+			nums1[retIndex] = nums2[j]
+			j--
+		} // else>>
+		retIndex--
+	} // for>
 }
