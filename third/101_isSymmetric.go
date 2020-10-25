3956205a6b099ef43c6eb5071ef0d2ea28509207
xushouming <xushouming@bytedance.com>
2020-10-25 22:25:36 +0800

    third start

---
+++ third/101_isSymmetric.go
@@ -0,0 +1,26 @@
+package leetcode_go_third
+
+func isSymmetric(root *TreeNode) bool {
+	u, v := root, root
+	queue := []*TreeNode{u, v}
+	for len(queue) > 0 {
+		u, v = queue[0], queue[1]
+		queue = queue[2:]
+		if u == nil && v == nil {
+			continue
+		} // if>>
+		if u == nil || v == nil {
+			return false
+		}
+		if u.Val != v.Val {
+			return false
+		}
+
+		queue = append(queue, u.Left)
+		queue = append(queue, v.Right)
+		queue = append(queue, u.Right)
+		queue = append(queue, v.Left)
+	} // for>
+
+	return true
+}
---
+++ third/104_maxDepth.go
@@ -0,0 +1,25 @@
+package leetcode_go_third
+
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	var depth int
+	queue := []*TreeNode{root}
+	for len(queue) > 0 {
+		depth++
+		curLevelLen := len(queue)
+		for i := 0; i < curLevelLen; i++ {
+			node := queue[0]
+			queue = queue[1:]
+			if node.Left != nil {
+				queue = append(queue, node.Left)
+			} // if>>>
+			if node.Right != nil {
+				queue = append(queue, node.Right)
+			} // if>>>
+		} // for>>
+	} // for>
+	return depth
+}
