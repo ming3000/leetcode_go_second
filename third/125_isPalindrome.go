e2050422defaf6bb32ebb065e7242cdf3aa40f5a
xushouming <xushouming@bytedance.com>
2020-10-25 22:39:40 +0800

    third start

---
+++ third/125_isPalindrome.go
@@ -0,0 +1,31 @@
+package leetcode_go_third
+
+import "strings"
+
+func isPalindrome(s string) bool {
+	s = strings.TrimSpace(s)
+	s = strings.ToLower(s)
+	bArr := make([]byte, 0, len(s))
+	for i := 0; i < len(s); i++ {
+		if isValidChar(s[i]) {
+			bArr = append(bArr, s[i])
+		} // if>>
+	} // for>
+
+	for i := 0; i < len(bArr)/2; i++ {
+		if bArr[i] != bArr[len(bArr)-1-i] {
+			return false
+		} // if>>
+	} // for>
+	return true
+}
+
+func isValidChar(c byte) bool {
+	if c >= '0' && c <= '9' {
+		return true
+	}
+	if c >= 'a' && c <= 'z' {
+		return true
+	}
+	return false
+}
