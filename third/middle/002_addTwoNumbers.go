0840bc8c92b4aef65d48df8226e7ed4a3a00417d
xushouming <xushouming@bytedance.com>
2020-10-26 08:11:14 +0800

    third middle

---
+++ third/middle/002_addTwoNumbers.go
@@ -0,0 +1,29 @@
+package easy
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	if l1 == nil && l2 == nil {
+		return nil
+	}
+	sentry := &ListNode{}
+	cur := sentry
+	carry := 0
+	for l1 != nil || l2 != nil || carry != 0 {
+		curSum := carry
+		if l1 != nil {
+			curSum += l1.Val
+			l1 = l1.Next
+		}
+		if l2 != nil {
+			curSum += l2.Val
+			l2 = l2.Next
+		}
+		newNode := &ListNode{Val: curSum % 10}
+
+		cur.Next = newNode
+		cur = cur.Next
+
+		carry = curSum / 10
+	} // for>
+
+	return sentry.Next
+}
