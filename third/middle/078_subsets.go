ad4dc9584c1f2a7373e2aa7fbc37d749f8482893
xushouming <xushouming@bytedance.com>
2020-10-27 11:34:12 +0800

    third middle

--- third/middle/078_subsets.go
+++ third/middle/078_subsets.go
@@ -7,9 +7,10 @@ func subsets(nums []int) [][]int {
 }
 
 func dfsSubsets(nums []int, start int, path []int, res *[][]int) {
-	if len(path) != 0 {
-		*res = append(*res, path)
-	}
+	temp := make([]int, len(path))
+	copy(temp, path)
+	*res = append(*res, temp)
+
 	for i := start; i < len(nums); i++ {
 		path = append(path, nums[i])
 		dfsSubsets(nums, i+1, path, res)
