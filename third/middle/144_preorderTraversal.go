1e693db798ba88cf3ff63240f604c1ee5b8cf28a
xushouming <xushouming@bytedance.com>
2020-10-27 17:12:54 +0800

    third middle

---
+++ third/middle/144_preorderTraversal.go
@@ -0,0 +1,22 @@
+package middle
+
+func preorderTraversal(root *TreeNode) []int {
+	if root == nil {
+		return nil
+	}
+
+	ret := make([]int, 0)
+	stack := []*TreeNode{root}
+	for len(stack) > 0 {
+		node := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		ret = append(ret, node.Val)
+		if node.Right != nil {
+			stack = append(stack, node.Right)
+		} // if>>
+		if node.Left != nil {
+			stack = append(stack, node.Left)
+		} // if>>
+	} // for>
+	return ret
+}
---
+++ third/middle/209_minSubArrayLen.go
@@ -0,0 +1,28 @@
+package middle
+
+import "math"
+
+func minSubArrayLen(s int, nums []int) int {
+	left, right := 0, -1
+	sum, ret := 0, math.MinInt64
+
+	for left < len(nums) {
+		if right+1 < len(nums) && sum < s {
+			right++
+			sum += nums[right]
+		} else {
+			sum -= nums[left]
+			left++
+		} // else>>
+
+		if sum > s {
+			ret = maxInt(ret, right-left+1)
+		}
+	} // if>
+
+	if ret == math.MaxInt64 {
+		return 0
+	} else {
+		return ret
+	}
+}
