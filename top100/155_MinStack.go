2857d0edf1b90282e7ca58696090426b8c0ca667
xushouming <xushouming@bytedance.com>
2020-10-30 11:55:35 +0800

    top 100

---
+++ top100/155_MinStack.go
@@ -0,0 +1,35 @@
+package middle
+
+type MinStack struct {
+	data    []int
+	minData []int
+}
+
+func Constructor() MinStack {
+	return MinStack{
+		data:    make([]int, 0),
+		minData: make([]int, 0),
+	}
+}
+
+func (this *MinStack) Push(x int) {
+	this.data = append(this.data, x)
+	if len(this.minData) == 0 || this.minData[len(this.minData)-1] > x {
+		this.minData = append(this.minData, x)
+	} else {
+		this.minData = append(this.minData, this.minData[len(this.minData)-1])
+	}
+}
+
+func (this *MinStack) Pop() {
+	this.data = this.data[:len(this.data)-1]
+	this.minData = this.minData[:len(this.minData)-1]
+}
+
+func (this *MinStack) Top() int {
+	return this.data[len(this.data)-1]
+}
+
+func (this *MinStack) GetMin() int {
+	return this.minData[len(this.minData)-1]
+}
