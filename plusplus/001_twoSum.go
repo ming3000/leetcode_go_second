793c44de25ec410c339fc75fc1b159c525d246a7
xushouming <xushouming@bytedance.com>
2020-11-02 11:45:25 +0800

    plus-plus

--- plusplus/001_twoSum.go
+++ plusplus/001_twoSum.go
@@ -1,5 +1,14 @@
 package plusplus
 
+// 这里我们可以增加一个 Map 记录已经遍历过的数字及其对应的索引值。
+// 这样当遍历一个新数字的时候就去 Map 里查询 target 与该数的差值是否已经在前面的数字中出现过。
+// 如果出现过，就找到了答案，就不必再往下继续执行了。
+//
+// 关键点
+// 求和转换为求差
+// 借助 Map 结构将数组中每个元素及其索引相互对应
+// 以空间换时间，将查找时间从 O(N) 降低到 O(1)
+
 func twoSum(nums []int, target int) []int {
 	cm := make(map[int]int, len(nums))
 	for i, v := range nums {
---
+++ plusplus/common.go
@@ -0,0 +1,28 @@
+package plusplus
+
+// Definition for singly-linked list.
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+// Definition for a binary tree node.
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func maxInt(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func minInt(a, b int) int {
+	if a > b {
+		return b
+	}
+	return a
+}
