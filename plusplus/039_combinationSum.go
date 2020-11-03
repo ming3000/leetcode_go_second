29cd90166313520f1bc92a37eed7bd9bd9c9005e
xushouming <xushouming@bytedance.com>
2020-11-03 23:31:29 +0800

    plus-plus

---
+++ plusplus/039_combinationSum.go
@@ -0,0 +1,25 @@
+package plusplus
+
+func combinationSum(candidates []int, target int) [][]int {
+	ret := make([][]int, 0)
+	dfsCombinationSum(candidates, target, 0, nil, &ret)
+	return ret
+}
+
+func dfsCombinationSum(nums []int, target, start int, path []int, ret *[][]int) {
+	if target < 0 {
+		return
+	}
+	if target == 0 {
+		temp := make([]int, len(path))
+		copy(temp, path)
+		*ret = append(*ret, temp)
+		return
+	}
+
+	for i := start; i < len(nums); i++ {
+		path = append(path, nums[i])
+		dfsCombinationSum(nums, target-nums[i], i, path, ret)
+		path = path[:len(path)-1]
+	}
+}
